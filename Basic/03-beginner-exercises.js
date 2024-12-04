9e0bb3edbcde8c488eb771bed6e7d8bc5da47ea4
LuisGH18 <lguanilo270@gmail.com>
2024-12-04 14:58:39 -0500

    03-beginner-exercises completed✅

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,21 +4,73 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
+// comentario de una linea✅
 
 // 2. Escribe un comentario en varias líneas
+/* Esto es
+comentario de
+varias lineas✅
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+var nombre = "Luis"
+var edad = 20
+var isStudent = true
+var undefinedValue
+var sinvalor = null
+//✅
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(nombre)
+console.log(edad)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(sinvalor)
+//✅
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(typeof(nombre))
+console.log(typeof(edad))
+console.log(typeof(isStudent))
+console.log(typeof(undefinedValue))
+console.log(typeof(sinvalor))
+//✅
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+nombre = "Gerardo"
+edad = 21
+isStudent = false
+//✅
+console.log(nombre)
+console.log(edad)
+console.log(isStudent)
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+nombre = 20
+edad = "Luis"
+isStudent = 1
+//✅
+console.log(typeof(nombre))
+console.log(typeof(edad))
+console.log(typeof(isStudent))
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const name = "Luis"
+const year = 20
+const isTeacher = true
+//const undefinedValue
+const nullValue = null
+//✅
+console.log(name)
+console.log(year)
+console.log(isTeacher)
+console.log(nullValue)
 
 // 9. A continuación, modifica los valores de las constantes
+//const name = "Gerardo"
+//const year = 21
+//const isTeacher = false
+//✅
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+//✅
